fb55f676f72857c6beaece2067afa30601afdd27
Shreyansh Jain <shreyjay0@gmail.com>
2022-03-26 07:11:18 +0530

    Revert "tailwind custom rgba fix/support"
    This reverts commit 8110e2e83d940a7ad40d76ce1789afd59215e7c5.

--- src/hooks/useKeyPress.tsx
+++ src/hooks/useKeyPress.tsx
@@ -1,1 +1,23 @@
-export {};
+import React, { useCallback, useEffect } from 'react';
+
+type KeyPressType = {
+  key: string;
+};
+
+function useKeyPress() {
+  const handleKeyPress = useCallback((event: KeyboardEvent) => {
+    const keyPressed: KeyPressType = {
+      key: event.key,
+    };
+    return keyPressed;
+  }, []);
+  return handleKeyPress;
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
+}
+
+export default useKeyPress;
